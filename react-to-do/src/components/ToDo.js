07700894b65e36e0b40fe7501263e2f09516ab75
Bealonzo <alonzo.briane@gmail.com>
2019-01-28 11:19:26 -0500

    assigment-52: Complete

--- react-to-do/src/components/ToDo.js
+++ react-to-do/src/components/ToDo.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
- class ToDo extends Component {
-   render() {
-     return (
-       <li>{this.props.description}</li>
-     );
-   }
+class ToDo extends Component {
+	deleteTodo(e) {
+   e.preventDefault()
+   this.props.deleteTodo(this.props.description);
  }
- export default ToDo;
+	render() {
+		return (
+			<li>
+				<input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+				<input value="Delete" type="button" onClick= {(e) => this.deleteTodo(e)} />
+				<span>{ this.props.description }</span>
+			</li>
+			);
+		}
+	}
+
+export default ToDo;
